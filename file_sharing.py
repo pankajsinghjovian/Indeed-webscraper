5c14434c21efeef2e8658c1a28c905ba92011ec9
pankajsinghjovian <136099763+pankajsinghjovian@users.noreply.github.com>
2023-06-15 16:27:00 +0530

    Update file_sharing.py

--- file_sharing.py
+++ file_sharing.py
@@ -3,30 +3,29 @@ from slack_sdk.errors import SlackApiError
 import os 
 
 
-slack_scrapper_token = os.getenv('slack_scraper_token')
 
-print(slack_scrapper_token)
+slack_scrapper_token = os.getenv('slack_scraper_token')
+channel =os.getenv("channel_id")
 
 
 # Set the path to the file you want to send
 file_path = r"JOBS.CSV"
-
+file_path1= r'JOBS.xlsx'
+file_list= [file_path1, file_path]
 
 # #Setting up an Automated Text
-message ="Here is the scrapped Jobs"
+message ="Here is the Indeed scrapped Jobs"
 
 # Initialize the Slack WebClient
 client = WebClient(token=slack_scrapper_token)
-channel =os.getenv("channel_id")
+
 #Function to send the file
 def send_file():
-     response =client.files_upload_v2(
-    
-        channel = channel,
-        file= file_path
-    )
-    
-
+     for file in file_list:
+         response =client.files_upload_v2(
+            channel = channel,
+            file= file,
+         )
      if response['ok']:
         print (f"File sent successful")
     
